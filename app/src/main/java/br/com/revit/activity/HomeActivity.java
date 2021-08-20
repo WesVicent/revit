c8b6962dd33bac048f60a9c783926e68ba507df8
Diogo Santos <diogo_2704@hotmail.com>
2021-08-20 17:10:07 -0300

    commit

--- app/src/main/java/br/com/revit/activity/HomeActivity.java
+++ app/src/main/java/br/com/revit/activity/HomeActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import br.com.revit.R;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,13 +14,15 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_home);
+
         Button btn = findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Toast.makeText(HomeActivity.this, "Ola", Toast.LENGTH_LONG).show();
             }
         });
 
-        setContentView(R.layout.activity_home);
     }
 }
