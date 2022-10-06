5e989c1706be1b49597abce0fb79162449fcc2df
Wesley Vicente <wesley.soften@gmail.com>
2022-10-05 21:06:18 -0300

    Removendo func de animações
    Adicionando docFuncional.md

--- app/src/main/java/com/study/wes/revit/views/fragments/MainFragment.kt
+++ app/src/main/java/com/study/wes/revit/views/fragments/MainFragment.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.AccelerateInterpolator
-import android.view.animation.Animation
-import android.view.animation.TranslateAnimation
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import com.study.wes.revit.R
@@ -20,54 +17,6 @@ class MainFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    }
-
-    private fun inFromRightAnimation(): Animation? {
-        val inFromRight: Animation = TranslateAnimation(
-            Animation.RELATIVE_TO_PARENT, +1.0f,
-            Animation.RELATIVE_TO_PARENT, 0.0f,
-            Animation.RELATIVE_TO_PARENT, 0.0f,
-            Animation.RELATIVE_TO_PARENT, 0.0f
-        )
-        inFromRight.duration = 500
-        inFromRight.interpolator = AccelerateInterpolator()
-        return inFromRight
-    }
-
-    private fun outToLeftAnimation(): Animation? {
-        val outtoLeft: Animation = TranslateAnimation(
-            Animation.RELATIVE_TO_PARENT, 0.0f,
-            Animation.RELATIVE_TO_PARENT, -1.0f,
-            Animation.RELATIVE_TO_PARENT, 0.0f,
-            Animation.RELATIVE_TO_PARENT, 0.0f
-        )
-        outtoLeft.duration = 1000
-        outtoLeft.interpolator = AccelerateInterpolator()
-        return outtoLeft
-    }
-
-    private fun inFromLeftAnimation(): Animation? {
-        val inFromLeft: Animation = TranslateAnimation(
-            Animation.RELATIVE_TO_PARENT, -1.0f,
-            Animation.RELATIVE_TO_PARENT, 0.0f,
-            Animation.RELATIVE_TO_PARENT, 0.0f,
-            Animation.RELATIVE_TO_PARENT, 0.0f
-        )
-        inFromLeft.duration = 1000
-        inFromLeft.interpolator = AccelerateInterpolator()
-        return inFromLeft
-    }
-
-    private fun outToRightAnimation(): Animation? {
-        val outtoRight: Animation = TranslateAnimation(
-            Animation.RELATIVE_TO_PARENT, 0.0f,
-            Animation.RELATIVE_TO_PARENT, +1.0f,
-            Animation.RELATIVE_TO_PARENT, 0.0f,
-            Animation.RELATIVE_TO_PARENT, 0.0f
-        )
-        outtoRight.duration = 500
-        outtoRight.interpolator = AccelerateInterpolator()
-        return outtoRight
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
